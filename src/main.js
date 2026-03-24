270f0403a3cea387d547d7dbabc54c975a68e69e
Ali Reza Baqeri <alirezabaqeri972@gmail.com>
2026-03-24 11:25:31 +0100

    create header & navbar

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@
 // import javascriptLogo from './assets/javascript.svg'
 // import { setupCounter } from './counter.js'
 
-console.log('Hello, World!');
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
 
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('nav-open');
+});
